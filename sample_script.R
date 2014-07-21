82cb3ce7d2bb451c5113069fd503f755c7ae061b
Gina Schmalzle <ginaschmalzle@gmail.com>
2014-07-21 13:39:15 -0700

    Modify with comments.  Update budgets.

--- sample_script.R
+++ sample_script.R
@@ -10,16 +10,16 @@ setwd('/Users/ginaschmalzle/Documents/Craig_R')
 # to the total number in the store).
 R <- 0.01
 # Assign the total number of iterations for sampled datasets
-it <- 100
+it <- 200
 # Assign year 2 budget 
 bd = 1000
 # Assign number of shoes
-nshoe1 <- 1000               # Number of shoes in the store in year 1
+nshoe1 <- 1000            # Number of shoes in the store in year 1
 meanprice1 <- 100         # Mean price of shoes in year 1
 pricesd1 <- 50            # Std of price in shoes in year 1
 
-nshoe2 <- 1400               # Number of shoes in the store in year 2
-meanprice2 <- 120            # Mean price of shoes in year 2          
+nshoe2 <- 1400            # Number of shoes in the store in year 2
+meanprice2 <- 120         # Mean price of shoes in year 2          
 pricesd2 <- 40            # Std of price in shoes in year 2
 #############################################################################
 
@@ -56,20 +56,24 @@ storesamples<-function(iteration, df, sr){
 shoesIcanbuy <- function(dataframe,mypurse){
   numofshoepairs <- 0
   while (mypurse > 0)  {
-    Shoe.pair<-dataframe[sample(nrow(dataframe),1),]
-    if (mypurse >= Shoe.pair$bucks){
-      mypurse<-mypurse-Shoe.pair$bucks
-      numofshoepairs <- numofshoepairs + 1 
-    }  
+    Shoe.pair<-dataframe[sample(nrow(dataframe),1),] # Pick a random pair of shoes
+    if (mypurse >= Shoe.pair$bucks){                 # As long as I have enough money in my purse
+      mypurse<-mypurse-Shoe.pair$bucks               # Buy a pair of shoes and subtract them from my budget
+      numofshoepairs <- numofshoepairs + 1           # Record the number of shoes I bought
+    } 
+    else {
+      break
+    }
   }
-  return(numofshoepairs)
+  return(numofshoepairs)                             # Return the number of shoes I bought
 }
 
-how_many_shoes_in_store_I_bought <- function(dataframe, summarya){
-  numofshoepairs <- array()
-  for (i in 1:nrow(summarya)) {
-    mypurse<-summarya[i,3]
-    for (j in 1:100){
+# Figure out how many shoes in the store I can buy given last year's budget.
+how_many_shoes_in_store_I_bought <- function(dataframe, summarya, it){
+  numofshoepairs <- array()                                   # Declare an array
+  for (i in 1:nrow(summarya)) {                               # Use each row in summarya as my starting budget
+    mypurse<-summarya[i,2]
+    for (j in 1:(2*it)){                                          # Figure out how many shoes I bought with each starting budget
       numofshoepairs[j] <- shoesIcanbuy(dataframe, mypurse)
     }
     numofshoepairs.df<-data.frame(Shoes=numofshoepairs)
@@ -78,41 +82,71 @@ how_many_shoes_in_store_I_bought <- function(dataframe, summarya){
   }
   return(numofshoepairs.masterdf)
 }
-############  END PART !
-#########START PART 2 - Given a total day budget of bd bucks, 
-#                       and given the number of bucks in summarya, what
-#                       sampling fraction can be expected from df2 data?
-#steps 
-#0 find your budget in bucks= BD-summarya$bucks in each interation
-# sum the number of bucks in df2
-#1. start with row 1 in summarya
-#2. select a pair of shoes from df2
-#3. select bucks in step 2 and subtract from budget
-#4. add 1 to the number of shoes already selected and save
-#5. evaluate new budget against 0; if <= 0 stop else goto step 2
-#Step 0:
 
+##############################################
+# END OF FUNCTIONS
+##############################################
 
-# make data takes three inputs: number of shoes, mean, and sd)
-df1 <- makedata(nshoe1, meanprice1, pricesd1)
-df2 <- makedata(nshoe2, meanprice2, pricesd2)
-a <- storesamples(it,df1,R)
+# Now for the questions...
+##############################################
+# QUESTION 1
+##############################################
+# In year 1, there were nshoe1 number of shoes in the store, that had a mean price of
+# meanprice1, and a price standard deviation of pricesd1.  How much money do I have to 
+# spend if I purchase 1% (R = 0.01) of the store's inventory?
+##############################################
+
+# First, make a fake dataset 
+# makedata takes three inputs: number of shoes, mean, and sd)
+# shoesinstore1 is a dataframe
+shoesinstore1 <- makedata(nshoe1, meanprice1, pricesd1)
+
+# moneyIspent collects R (in this case 1%) shoes of the store's inventory
+# and figures out how much money I spent in each case.
+moneyIspent <- storesamples(it,shoesinstore1,R)
 
 ## Summarize output
-summarya <- ddply(a, .(index), summarize, Totalbucks = floor(sum(bucks)))
+# summarya provides the mean, median, 1st and 3rd quartiles, and min and max money 
+# I spent given the samples in moneyIspent.
+summarya <- ddply(moneyIspent, .(index), summarize, Totalbucks = floor(sum(bucks)))
+
+# Plot a histogram of the money I spent
 (ggplot(summarya, aes(x=Totalbucks))
  + geom_histogram()
 )
+
+#Print a summary of the amount of money I could spend. 
 summary(summarya$Totalbucks)
-###Summarize the outcome as a rate
-#summarya$Available.bucks <- bd-summarya$Totalbucks
-#numofshoepairs.masterdf <- how_many_shoes_in_store_I_bought(df2,summarya)
-#numofshoepairs.masterdf$Rate<-numofshoepairs.masterdf$Shoes/nrow(df2)
-#(ggplot(numofshoepairs.masterdf, aes(x=Rate))
-# + geom_histogram(aes(y=..density..), fill="gray", color="black")
-# + theme_bw()
+
+##############################################
+# QUESTION 2
+##############################################
+# In year 2, I was given the same amount of money I spent in year 1 as my budget.  
+# What percentage of the store's inventory I can buy in year 2, 
+# given the amount of money I spent in year 1?  
+# Hint:  I will take the sampled money I spent last year and figure out 
+# how many shoes I can buy with that money.
+##############################################
+
+# Make another dataset for year 2 
+shoesinstore2 <- makedata(nshoe2, meanprice2, pricesd2)
+# Collect information on how many shoes I bought, and the corresponding
+# Percentage of how many shoes I bought in the store.
+numofshoepairs.masterdf <- how_many_shoes_in_store_I_bought(shoesinstore2,summarya,it)
+# Calculate a percent by taking the number of shoes I bought and dividing it by the corresponding 
+# Number of shoes in the store, and multiplying by 100.
+numofshoepairs.masterdf$Percent<-(numofshoepairs.masterdf$Shoes/nrow(shoesinstore2))*100
+
+# Plot a histogram of the percentage of shoes in the store I bought 
+(ggplot(numofshoepairs.masterdf, aes(x=Percent))
+ + geom_histogram(aes(y=..density..), fill="gray", color="black", binwidth = .1)
+ + theme_bw()
 # + geom_density(color="red")
-# + geom_vline(x=mean(numofshoepairs.masterdf$Rate), color="blue")
-#)
-#summary(numofshoepairs.masterdf$Rate)
-#summary(numofshoepairs.masterdf$Shoes)
+ + geom_vline(x=mean(numofshoepairs.masterdf$Percent), color="blue")
+)
+
+# Print summary of the percentage of shoes I bought from the store
+summary(numofshoepairs.masterdf$Percent)
+
+# Print summary of the number of shoes I bought given my budget.
+summary(numofshoepairs.masterdf$Shoes)
